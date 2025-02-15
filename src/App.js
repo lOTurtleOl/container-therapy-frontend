161695397aefc430fe8e63a06b237c1215c4abbf
Andrew Hughes <andyashmore1@gmailcom>
2025-02-15 17:15:13 -0600

    fix imports and nav dropdown

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Nav from './components/Nav';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 
 
 export default function App() {
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import {
     BrowserRouter as Router,
     Route,
     Routes,
-    Link
+    NavLink
   } from "react-router-dom";
   import HomePage from '../HomePage';
   import CreationPage from '../CreationPage';
@@ -16,20 +16,20 @@ export default function Nav() {
       <div>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
           <div className="container-fluid">
-            <a className="navbar-brand" href="#">Navbar</a>
+            <a className="navbar-brand" href="/">Navbar</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav">
                 <li className="nav-item">
-                  <Link to="/">Home</Link>
+                  <NavLink className="nav-link" to="/">Home</NavLink>
                 </li>
                 <li className="nav-item">
-                  <Link to="/create">Creation Page</Link>
+                  <NavLink className="nav-link" to="/create">Creation Page</NavLink>
                 </li>
                 <li className="nav-item">
-                  <Link to="/container">Container Page</Link>
+                  <NavLink className="nav-link" to="/container">Container Page</NavLink>
                 </li>
               </ul>
             </div>
